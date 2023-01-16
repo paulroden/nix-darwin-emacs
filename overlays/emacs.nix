80bc40117aeb7f3dc9d25a1b74d9e13edaf17374
Paul Roden <git@paulroden.net>
2023-01-16 17:37:39 +0000

    oh yeah, one more level in that path

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -38,7 +38,7 @@ let
                 
                 cat <<<'#! /usr/bin/env nix-shell
 #! nix-shell -i bash -p bash
-exec ${placeholder "out"}/Emacs.app/Contents/MacOS/Emacs "$@"
+exec ${placeholder "out"}/Applications/Emacs.app/Contents/MacOS/Emacs "$@"
 ' > $out/bin/emacs
               '';
 
